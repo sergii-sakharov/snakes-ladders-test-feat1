963ef12e14cea2d353cb7f5805bfd7bfbb5db631
sergii.sakharov <sergii.sakharov@reedonline.co.uk>
2019-02-27 07:24:00 +0000

    init

---
+++ SnakesAndLadders.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace SnakesAndLadders.Tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void PlayerRegistered_ShouldBeOnPosition1()
+        {
+
+
+        }
+    }
+}
